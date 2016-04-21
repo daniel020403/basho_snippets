42fb0c321965c5c185db6d5f3b02e1a6b534cc1d
Daniel Garcia <danielgarcia@Daniels-MacBook-Pro.local>
2016-04-21 10:52:43 +0800

    Added snippets for RT helper leak issues.

---
+++ rt_helper_leak.erl
@@ -0,0 +1,18 @@
+%% issue: https://github.com/basho/riak_repl/issues/649
+
+%% ----- sink -----
+
+%% check for orphaned helper processes
+[ Pid || {Pid, false} <- [ {Pid, is_process_alive(Parent)} || {Pid, {riak_repl2_rtsink_helper,init,1}, [Parent|Others]} <- [ {Pid, proplists:get_value('$initial_call', D), proplists:get_value('$ancestors',D)} || {Pid,{dictionary,D}} <- [ {P,process_info(P,dictionary)} || P <- processes() ]]] ].
+
+%% remove orphans
+[ exit(Pid, orphan) || {Pid, false} <- [ {Pid, is_process_alive(Parent)} || {Pid, {riak_repl2_rtsink_helper,init,1}, [Parent|Others]} <- [ {Pid, proplists:get_value('$initial_call', D), proplists:get_value('$ancestors',D)} || {Pid,{dictionary,D}} <- [ {P,process_info(P,dictionary)} || P <- processes() ]]] ].
+
+
+%% ----- source -----
+
+%% check for orphaned helper processes
+[ Pid || {Pid, false} <- [ {Pid, is_process_alive(Parent)} || {Pid, {riak_repl2_rtsource_helper,init,1}, [Parent|Others]} <- [ {Pid, proplists:get_value('$initial_call', D), proplists:get_value('$ancestors',D)} || {Pid,{dictionary,D}} <- [ {P,process_info(P,dictionary)} || P <- processes() ]]] ].
+
+%% remove orphans
+[ exit(Pid, orphan) || {Pid, false} <- [ {Pid, is_process_alive(Parent)} || {Pid, {riak_repl2_rtsource_helper,init,1}, [Parent|Others]} <- [ {Pid, proplists:get_value('$initial_call', D), proplists:get_value('$ancestors',D)} || {Pid,{dictionary,D}} <- [ {P,process_info(P,dictionary)} || P <- processes() ]]] ].
