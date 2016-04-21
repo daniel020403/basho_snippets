db315f5223d1329a1ea4071beb5cac7cf655b877
Daniel Garcia <danielgarcia@Daniels-MacBook-Pro.local>
2016-04-21 15:03:27 +0800

    Combined snippets for rt helper leak.

--- rt_helper_leak.erl
+++ rt_helper_leak.erl
@@ -16,3 +16,12 @@
 
 %% remove orphans
 [ exit(Pid, orphan) || {Pid, false} <- [ {Pid, is_process_alive(Parent)} || {Pid, {riak_repl2_rtsource_helper,init,1}, [Parent|Others]} <- [ {Pid, proplists:get_value('$initial_call', D), proplists:get_value('$ancestors',D)} || {Pid,{dictionary,D}} <- [ {P,process_info(P,dictionary)} || P <- processes() ]]] ].
+
+
+%% combining
+
+Kill = fun() ->
+{length([ exit(Pid, orphan) || {Pid, false} <- [ {Pid, is_process_alive(Parent)} || {Pid, {riak_repl2_rtsource_helper,init,1}, [Parent|Others]} <- [ {Pid, proplists:get_value('$initial_call', D), proplists:get_value('$ancestors',D)} || {Pid,{dictionary,D}} <- [ {P,process_info(P,dictionary)} || P <- processes() ]]] ]),
+length([ exit(Pid, orphan) || {Pid, false} <- [ {Pid, is_process_alive(Parent)} || {Pid, {riak_repl2_rtsink_helper,init,1}, [Parent|Others]} <- [ {Pid, proplists:get_value('$initial_call', D), proplists:get_value('$ancestors',D)} || {Pid,{dictionary,D}} <- [ {P,process_info(P,dictionary)} || P <- processes() ]]] ])}
+end.
+riak_core_util:rpc_every_member_ann(erlang, apply, [ Kill, []], 600000).
